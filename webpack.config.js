a3fe962ab4e83712a6ce36ac198c9518a54ceeb6
rbeazley18 <robertbeazley18@gmail.com>
2022-11-26 09:01:21 -0500

    removed font loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,6 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
-            // {
-            //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            //     type: 'asset/resource',
-            //     use: {
-            //         loader: 'file-loader'
-            //     }
-            // },
         ]
     }
 };
