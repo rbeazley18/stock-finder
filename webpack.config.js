68a73d3aedd8159c26be79fb87556b60070d1d38
rbeazley18 <robertbeazley18@gmail.com>
2022-11-27 11:28:47 -0500

    working on mongo

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
     },
+    resolve: {
+        conditionNames: ['require', 'node'],
+    },
     plugins: [
         new Dotenv(),
     ],
